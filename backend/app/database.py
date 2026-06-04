8089184e9b5ce90df30b1b03650918ac5d7dc25a
91720 <a91720@Ankitas-MacBook-Air.local>
2026-06-04 14:46:15 +0530

    Prepare application for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./talentiq.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
--- backend/app/main.py
+++ backend/app/main.py
@@ -55,9 +55,7 @@ app.include_router(
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=[
-        "http://localhost:3000"
-    ],
+    allow_origins=["*"],
 
     allow_credentials=True,
 
